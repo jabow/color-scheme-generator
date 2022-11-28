a5f5fa6f494ab0fd4ba923e2edb23ad3a1de9ac7
jabow <jdbowen1994@hotmail.com>
2022-11-28 01:52:55 +0000

    Added - favicon, fonts, copy hex values, dark mode

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ form.addEventListener("submit", function (e) {
 	e.preventDefault();
 	const color = colorInput.value.slice(1);
 	const mode = modeInput.value;
-	console.log(color + " " + mode);
 
 	const options = {
 		method: "GET",
@@ -28,8 +27,57 @@ form.addEventListener("submit", function (e) {
 function renderColors(colorArray) {
 	let count = 1;
 	for (color of colorArray) {
+		let hexValue = color.hex.value;
 		document.getElementById(`color${count}`).style.backgroundColor =
-			document.getElementById(`hex${count}`).innerText = color.hex.value;
+			hexValue;
+		document.getElementById(`hex${count}`).innerText = hexValue;
 		count++;
 	}
 }
+
+//Toggle between dark and light themes
+const btn = document.querySelector(".btn-toggle");
+btn.addEventListener("click", function () {
+	toggleDarkLightMode();
+});
+
+function toggleDarkLightMode() {
+	document.body.classList.toggle("dark-theme");
+	document.getElementById("theme").classList.toggle("fa-sun");
+	document.getElementById("theme").classList.toggle("fa-moon");
+}
+
+//Copy hex values when clicked
+function copy(hexValue) {
+	navigator.clipboard.writeText(hexValue.innerText);
+}
+
+getTheme();
+//Get users system theme setting
+
+function getTheme() {
+	if (
+		window.matchMedia &&
+		window.matchMedia("(prefers-color-scheme: dark)").matches
+	) {
+		// dark mode
+		toggleDarkLightMode();
+	}
+}
+
+//check for changes in users system theme setting
+window
+	.matchMedia("(prefers-color-scheme: dark)")
+	.addEventListener("change", (event) => {
+		//Check the current setting and only change if it doesnt match
+		if (event.matches && !document.body.classList.contains("dark-theme")) {
+			//dark
+			toggleDarkLightMode();
+		} else if (
+			!event.matches &&
+			document.body.classList.contains("dark-theme")
+		) {
+			//light
+			toggleDarkLightMode();
+		}
+	});
